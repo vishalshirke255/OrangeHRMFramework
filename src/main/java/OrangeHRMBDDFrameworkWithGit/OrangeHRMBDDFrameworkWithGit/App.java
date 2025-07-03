2b178dc6d285b562e00999b12a247d609eca5ed6
vishalshirke255 <vishalshirke255@gmail.com>
2025-07-03 10:03:06 +0530

    Framework is developed

---
+++ src/main/java/OrangeHRMBDDFrameworkWithGit/OrangeHRMBDDFrameworkWithGit/App.java
@@ -0,0 +1,13 @@
+package OrangeHRMBDDFrameworkWithGit.OrangeHRMBDDFrameworkWithGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
